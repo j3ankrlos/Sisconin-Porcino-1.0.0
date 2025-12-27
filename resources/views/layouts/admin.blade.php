e99bd9ebb7243fb82706bcc90cfce645bec54199
jean colmenares <jeankrlos687@gmail.com>
2025-12-27 13:45:04 -0400

    Updated system version to 1.2.0 - Core access and security configured

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -440,7 +440,7 @@
     <strong>Copyright &copy; {{ date('Y') }} <a href="{{ route('admin.index') }}">SISCONINT</a>.</strong>
     Todos los derechos reservados.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Versión</b> 1.1.0
+      <b>Versión</b> 1.2.0
     </div>
   </footer>
 </div>
